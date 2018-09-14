a7e0a5f11a2fa115f1b391c9175b1529a9706b8e
Chien Wen Liu <dreamuk@gmail.com>
2018-09-14 11:28:25 +0800

    add checking possible move method

--- src/game.js
+++ src/game.js
@@ -7,43 +7,68 @@ export default class Game extends React.Component {
     super(props);
     this.state = {
       name: "",
-      puzzle: [1, 2, 3, 4, 5, 6, 7, 8, 0],
-      step: 0,
-      lastMove: ""
+      puzzle: [1, 2, 3, 4, 5, 6, 7, 0, 8],
+      step: 0
     };
     this.handleName = this.handleName.bind(this);
     this.movePiece = this.movePiece.bind(this);
     this.shuffle = this.shuffle.bind(this);
+    this.validSolution = this.validSolution.bind(this);
     this.checkPossibleMove = this.checkPossibleMove.bind(this);
+    this.swapPiece = this.swapPiece.bind(this);
   }
   handleName(e) {
-    this.setState({ name: e.currentTarget.value });
+    this.setState({name: e.currentTarget.value});
   }
-  movePiece(target) {
-    let emptyIndex = this.state.puzzle.findIndex(val => val === 0);
-    console.log("emptyIndex", emptyIndex);
-    console.log("current", target);
-    console.log(emptyIndex-target);
+  movePiece(targetIndex) {
+    let emptyIndex = this.state.puzzle.findIndex(piece => piece === 0);
 
+    if (targetIndex === emptyIndex) return;
+    if (this.checkPossibleMove(targetIndex)) {
+      this.swapPiece(targetIndex, emptyIndex);
+    }
+  }
+  swapPiece(targetIndex, emptyIndex) {
     let moved = this.state.puzzle.slice(0);
-    this.setState({puzzle:[]})
-
+    let targetPiece = moved[targetIndex];
+    moved[emptyIndex] = targetPiece;
+    moved[targetIndex] = 0;
+    this.setState({puzzle: moved, step: this.state.step + 1});
   }
-  checkPossibleMove(){
+  checkPossibleMove(targetIndex) {
+    let emptyIndex = this.state.puzzle.findIndex(piece => piece === 0);
+    let boardRowNum = 3;
+
+    // check if in same row, then check if in the same column;
+    return (
+      (Math.abs(targetIndex - emptyIndex) === boardRowNum &&
+                targetIndex % boardRowNum === emptyIndex % boardRowNum) ||
+            (Math.abs(targetIndex - emptyIndex) === 1 &&
+                Math.floor(targetIndex / boardRowNum) ===
+                    Math.floor(emptyIndex / boardRowNum))
+    );
   }
   shuffle() {}
+  validSolution() {
+    return (
+      this.state.puzzle
+        .slice(0, 8)
+        .filter((piece, index) => +piece !== index + 1).length === 0
+    );
+  }
   render() {
     let puzzle = this.state.puzzle.map((num, index) => (
       <div key={index} onClick={() => this.movePiece(index)}>
         <span> {num !== 0 ? num : ""}</span>
       </div>
     ));
-
+    let solvedText = this.validSolution() ? "你贏了" : "";
     return (
       <div>
         <input value={this.state.name} onChange={this.handleName} />
         <button disabled={!this.state.name.length}>start</button>
         <div>step count:{this.state.step}</div>
+        <div>{solvedText}</div>
         <div className="puzzle"> {puzzle}</div>
       </div>
     );
@@ -59,15 +84,13 @@ export class Rank extends React.Component {
   }
 }
 
+// let target;
+// if(dir === "up"){
 
+// } else if (dir === "down"){
 
-    // let target;
-    // if(dir === "up"){
+// } else if(dir ==="left"){
 
-    // } else if (dir === "down"){
+// } else if(dir === "right"){
 
-    // } else if(dir ==="left"){
-
-    // } else if(dir === "right"){
-
-    // }
+// }
