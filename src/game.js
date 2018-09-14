8e51cf29d74e6466d13540b4ec546832e03f72c7
Chien Wen Liu <dreamuk@gmail.com>
2018-09-14 12:04:32 +0800

    add shuffle card method

--- src/game.js
+++ src/game.js
@@ -7,13 +7,15 @@ export default class Game extends React.Component {
     super(props);
     this.state = {
       name: "",
-      puzzle: [1, 2, 3, 4, 5, 6, 7, 0, 8],
-      step: 0
+      puzzle: [1, 2, 3, 4, 5, 6, 7, 0],
+      step: 0,
+      isGameOver: false
     };
     this.handleName = this.handleName.bind(this);
     this.movePiece = this.movePiece.bind(this);
     this.shuffle = this.shuffle.bind(this);
     this.validSolution = this.validSolution.bind(this);
+    this.shuffle = this.shuffle.bind(this);
     this.checkPossibleMove = this.checkPossibleMove.bind(this);
     this.swapPiece = this.swapPiece.bind(this);
   }
@@ -40,6 +42,8 @@ export default class Game extends React.Component {
     let boardRowNum = 3;
 
     // check if in same row, then check if in the same column;
+    // piece in the same column should have same remaining, in the same row should have
+    // same quotient
     return (
       (Math.abs(targetIndex - emptyIndex) === boardRowNum &&
                 targetIndex % boardRowNum === emptyIndex % boardRowNum) ||
@@ -48,13 +52,34 @@ export default class Game extends React.Component {
                     Math.floor(emptyIndex / boardRowNum))
     );
   }
-  shuffle() {}
+  shuffle() {
+    const getRandomInt = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    let swapTimes = Math.floor(Math.random() * 1000) * 2;
+    let i = 0;
+    let puzzleArr = Array.from({length: 9})
+      .fill(0)
+      .map((piece, index) => index);
+    while (i < swapTimes) {
+      // handle swap boundary;
+      let randomIndex = getRandomInt(1, 8);
+      let prev = puzzleArr[randomIndex - 1],
+        current = puzzleArr[randomIndex];
+      puzzleArr[randomIndex] = prev;
+      puzzleArr[randomIndex - 1] = current;
+      i++;
+    }
+    this.setState({puzzle: puzzleArr});
+  }
   validSolution() {
-    return (
-      this.state.puzzle
-        .slice(0, 8)
-        .filter((piece, index) => +piece !== index + 1).length === 0
-    );
+    let isGameOver =
+            this.state.puzzle
+              .slice(0, 8)
+              .filter((piece, index) => +piece !== index + 1).length === 0;
+    if (isGameOver) {
+      this.setState({isGameOver: true});
+    }
   }
   render() {
     let puzzle = this.state.puzzle.map((num, index) => (
@@ -70,6 +95,7 @@ export default class Game extends React.Component {
         <div>step count:{this.state.step}</div>
         <div>{solvedText}</div>
         <div className="puzzle"> {puzzle}</div>
+        <button onClick={this.shuffle}>洗牌</button>
       </div>
     );
   }
